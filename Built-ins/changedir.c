54ad0ff8c422f617886683169676c9bcada4e90f
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-02-03 21:32:57 +0000

    updated chdir function to save old and new, handles ~ character as well as -

--- Built-ins/changedir.c
+++ Built-ins/changedir.c
@@ -4,18 +4,35 @@
 // Built in for change directory
 int change_directory (const char *pathname, char *old, size_t len)
 {
+    char *home = getenv("HOME");
+    char *old = getenv("OLDPWD");
+    char cwd[1024];
 
-    if (path) {
-        fprintf("cd $HOME")
-    }
     // self made strlen
-    if (path == NULL || my_strlen(path) == 0) 
+    if (path == NULL || my_strlen(path, "~") == 0)// defaults to the users home directory
     {
-        fprintf(stderr, "cd: missing argument\n");
-        return;
+        path = home;
+    } else if (my_strlen(path, "-") == 0) {// handles cd-
+        if (old != NULL) {
+            path = old;// changes previous directory in OLDPWD
+            printf("%s\n", old);
+        } else {
+            fprintf(stderr, "cd: missing argument\n");
+            return;
+        }
     }
-    if (chdir(path) != 0) {
-        perror("cd");
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {// saves current dir as OLD
+        setenv("OLDPWD", cwd, 1);
+    }
+
+    if (chdir(path) == 0) {// saves new dir using getcwd and setenv
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            setenv("PWD", cwd, 1);
+            print("Changed directory to %s\n", cwd);
+        }
+    } else {
+    perror("cd");
     }
 }
 // Ariel
